01234b4cd8785982e39991a74b40bc0f58eb78c8
Rishikesh Chandra <rishikesh@azuratech.in>
2019-11-04 16:06:05 +0530

    Added new build

---
+++ build/precache-manifest.e7b100d6aaa10c16323cbc3ba19f3628.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "380af845f97462074a6002f52f704672",
+    "url": "/static/media/favicon.380af845.png"
+  },
+  {
+    "revision": "acf9968988dcd12dd2d1",
+    "url": "/static/css/main.2858bf6a.chunk.css"
+  },
+  {
+    "revision": "33682851687e9aeea404",
+    "url": "/static/js/1.33682851.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba78cc8100ece6278e5d015658f97b90",
+    "url": "/static/media/t2.ba78cc81.png"
+  },
+  {
+    "revision": "991479860720b8c59d95b55a19f8c05f",
+    "url": "/static/media/t3.99147986.png"
+  },
+  {
+    "revision": "acf9968988dcd12dd2d1",
+    "url": "/static/js/main.acf99689.chunk.js"
+  },
+  {
+    "revision": "ee6c980745b1d223bfa8e74de798106e",
+    "url": "/static/media/rishi.ee6c9807.jpg"
+  },
+  {
+    "revision": "075df2cb9cf2173d97fea9e566d36f38",
+    "url": "/static/media/wrk.075df2cb.jpeg"
+  },
+  {
+    "revision": "c57034ed6bc089f1e55017940d7cd16f",
+    "url": "/static/media/netflix-logo.c57034ed.png"
+  },
+  {
+    "revision": "efa1f9ee67a49af0682765d80e363523",
+    "url": "/static/media/work.efa1f9ee.jpg"
+  },
+  {
+    "revision": "936fdb6d515c31256866799d7194da21",
+    "url": "/static/media/place.936fdb6d.jpg"
+  },
+  {
+    "revision": "3d32e766f18b9e479401a840edb52d79",
+    "url": "/index.html"
+  }
+];
